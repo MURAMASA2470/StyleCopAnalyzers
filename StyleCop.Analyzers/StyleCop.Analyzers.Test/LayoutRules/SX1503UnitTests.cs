ff20ef736ed85a281e19c2681418a207cb242173
MURAMASA2470 <keisuke226214@gmail.com>
2023-02-14 01:44:07 +0900

    既存のアナライザをカスタマイズ

---
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test/LayoutRules/SX1503UnitTests.cs
@@ -0,0 +1,161 @@
+﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#nullable disable
+
+namespace StyleCop.Analyzers.Test.LayoutRules
+{
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis.Testing;
+    using Microsoft.CodeAnalysis.Text;
+    using StyleCop.Analyzers.LayoutRules;
+    using Xunit;
+    using static StyleCop.Analyzers.Test.Verifiers.StyleCopCodeFixVerifier<
+        StyleCop.Analyzers.LayoutRules.SX1503BracesMustNotBeOmitted,
+        StyleCop.Analyzers.LayoutRules.SX1503CodeFixProvider>;
+
+    /// <summary>
+    /// Unit tests for <see cref="SX1503BracesMustNotBeOmitted"/>.
+    /// </summary>
+    public class SX1503UnitTests
+    {
+        /// <summary>
+        /// Gets the statements that will be used in the theory test cases.
+        /// </summary>
+        /// <value>
+        /// The statements that will be used in the theory test cases.
+        /// </value>
+        public static IEnumerable<object[]> TestStatements
+        {
+            get
+            {
+                yield return new[] { "if (i == 0)" };
+                yield return new[] { "while (i == 0)" };
+                yield return new[] { "for (var j = 0; j < i; j++)" };
+                yield return new[] { "foreach (var j in new[] { 1, 2, 3 })" };
+                yield return new[] { "lock (this)" };
+                yield return new[] { "using (this)" };
+                yield return new[] { "fixed (byte* ptr = new byte[10])" };
+            }
+        }
+
+        /// <summary>
+        /// Verifies that a <c>do</c> statement followed by a block without braces will produce a warning, and the
+        /// code fix for this warning results in valid code.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Fact]
+        public async Task TestDoStatementAsync()
+        {
+            var testCode = @"using System.Diagnostics;
+public class Foo
+{
+    public void Bar(int i)
+    {
+        do
+            Debug.Assert(true);
+        while (false);
+    }
+}";
+            var fixedCode = @"using System.Diagnostics;
+public class Foo
+{
+    public void Bar(int i)
+    {
+        do
+        {
+            Debug.Assert(true);
+        }
+        while (false);
+    }
+}";
+
+            var expected = Diagnostic().WithLocation(7, 13);
+            await VerifyCSharpFixAsync(testCode, expected, fixedCode, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Verifies that an if / else statement followed by a block without braces will produce a warning.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Fact]
+        public async Task TestIfElseStatementWithoutBracesAsync()
+        {
+            var testCode = @"using System.Diagnostics;
+public class Foo
+{
+    public void Bar(int i)
+    {
+        if (i == 0)
+            Debug.Assert(true);
+        else
+            Debug.Assert(false);
+    }
+}";
+
+            var expected = new[]
+            {
+                Diagnostic().WithLocation(7, 13),
+                Diagnostic().WithLocation(9, 13),
+            };
+
+            await VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Verifies that an if statement followed by a block with braces will produce no diagnostics results.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Fact]
+        public async Task TestIfElseStatementWithBracesAsync()
+        {
+            var testCode = @"using System.Diagnostics;
+public class Foo
+{
+    public void Bar(int i)
+    {
+        if (true) return;
+        if (i == 0)
+        {
+            Debug.Assert(true);
+        }
+        else
+        {
+            Debug.Assert(false);
+        }
+    }
+}";
+
+            await VerifyCSharpDiagnosticAsync(testCode, DiagnosticResult.EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Verifies that an if statement followed by a block with braces will produce no diagnostics results.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Fact]
+        public async Task TestIfDirectiveTriviaAsync()
+        {
+            var testCode = @"using System.Diagnostics;
+public class Foo
+{
+    public void Bar(int i)
+    {
+        i == 0 ? Debug.Assert(true) : i != 0 ? Debug.Assert(true) : Debug.Assert(false);
+        //if (i == 0)
+        //{
+        //    Debug.Assert(true);
+        //}
+        //else
+        //{
+        //    Debug.Assert(false);
+        //}
+    }
+}";
+
+            await VerifyCSharpDiagnosticAsync(testCode, DiagnosticResult.EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
+    }
+}
