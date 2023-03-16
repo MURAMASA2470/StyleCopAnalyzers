a997acb963d0b019da29b2fe32e2f9f2f1d3aaef
MURAMASA2470 <keisuke226214@gmail.com>
2023-03-17 01:47:36 +0900

    カスタマイズ

---
+++ StyleCop.Analyzers/StyleCop.Analyzers/LayoutRules/SX1580TernaryOperatorsNotBeNested.cs
@@ -0,0 +1,104 @@
+﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#nullable disable
+
+namespace StyleCop.Analyzers.LayoutRules
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using Microsoft.CodeAnalysis.Diagnostics;
+    using StyleCop.Analyzers.Helpers;
+    using StyleCop.Analyzers.Settings.ObjectModel;
+
+    /// <summary>
+    /// The opening and closing braces for a C# statement have been omitted.
+    /// </summary>
+    /// <remarks>
+    /// <para>A violation of this rule occurs when the opening and closing braces for a statement have been omitted. In
+    /// C#, some types of statements may optionally include braces. Examples include <c>if</c>, <c>while</c>, and
+    /// <c>for</c> statements. For example, an if-statement may be written without braces:</para>
+    ///
+    /// <code language="csharp">
+    /// if (true)
+    ///     return this.value;
+    /// </code>
+    ///
+    /// <para>Although this is legal in C#, StyleCop always requires the braces to be present, to increase the
+    /// readability and maintainability of the code.</para>
+    ///
+    /// <para>When the braces are omitted, it is possible to introduce an error in the code by inserting an additional
+    /// statement beneath the if-statement. For example:</para>
+    ///
+    /// <code language="csharp">
+    /// if (true)
+    ///     this.value = 2;
+    ///     return this.value;
+    /// </code>
+    ///
+    /// <para>Glancing at this code, it appears as if both the assignment statement and the return statement are
+    /// children of the if-statement. In fact, this is not true. Only the assignment statement is a child of the
+    /// if-statement, and the return statement will always execute regardless of the outcome of the if-statement.</para>
+    ///
+    /// <para>StyleCop always requires the opening and closing braces to be present, to prevent these kinds of
+    /// errors:</para>
+    ///
+    /// <code language="csharp">
+    /// if (true)
+    /// {
+    ///     this.value = 2;
+    ///     return this.value;
+    /// }
+    /// </code>
+    /// </remarks>
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    internal class SX1580TernaryOperatorsNotBeNested : DiagnosticAnalyzer
+    {
+        /// <summary>
+        /// The ID for diagnostics produced by the <see cref="SX1580TernaryOperatorsNotBeNested"/> analyzer.
+        /// </summary>
+        public const string DiagnosticId = "SX1580";
+
+        private const string HelpLink = "https://github.com/DotNetAnalyzers/StyleCopAnalyzers/blob/master/documentation/SX1580.md";
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(LayoutResources.SA1503Title), LayoutResources.ResourceManager, typeof(LayoutResources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(LayoutResources.SA1503MessageFormat), LayoutResources.ResourceManager, typeof(LayoutResources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(LayoutResources.SA1503Description), LayoutResources.ResourceManager, typeof(LayoutResources));
+
+#pragma warning disable SA1202 // Elements should be ordered by access
+        internal static readonly DiagnosticDescriptor Descriptor =
+            new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, AnalyzerCategory.LayoutRules, DiagnosticSeverity.Warning, AnalyzerConstants.EnabledByDefault, Description, HelpLink);
+#pragma warning restore SA1202 // Elements should be ordered by access
+
+        /// <inheritdoc/>
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } =
+            ImmutableArray.Create(Descriptor);
+
+        /// <inheritdoc/>
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+
+            context.RegisterSyntaxNodeAction(ctx => CheckChildExpression(ctx, ((ConditionalExpressionSyntax)ctx.Node)), SyntaxKind.ConditionalExpression);
+        }
+
+        private static void CheckChildExpression(SyntaxNodeAnalysisContext context, ExpressionSyntax childExpression)
+        {
+            if (childExpression is ConditionalExpressionSyntax child)
+            {
+                if (child.Parent is ConditionalExpressionSyntax)
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(Descriptor, childExpression.GetLocation()));
+                }
+            }
+
+            return;
+        }
+    }
+}
